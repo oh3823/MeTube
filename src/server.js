e2bc6ef19a0e2ac94add7b9e441c5e6bba2eb522
oh3823 <ph38230119@gmail.com>
2023-03-16 16:41:59 +0900

    fixed

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.use(
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL_REMOTE }),
   })
 );
 app.use((req, res, next) => {
